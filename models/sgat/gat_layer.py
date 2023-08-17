8275d55fdd3c379f57d7bb81b7de31e95016135e
DimuthuLakmal <kjtdimuthu.13@cse.mrt.ac.lk>
2023-08-17 20:07:14 +1000

    Made a change to GAT

--- models/sgat/gat_layer.py
+++ models/sgat/gat_layer.py
@@ -13,9 +13,9 @@ class GATLayer(nn.Module):
         self.n_heads = n_heads
 
         if edge_dim == -1:
-            self.conv = GATv2Conv(in_features, out_features, heads=n_heads, dropout=dropout, concat=concat)
+            self.conv = GATv2Conv(in_features, out_features, heads=n_heads, dropout=dropout, concat=concat, aggr='mean')
         else:
-            self.conv = GATv2Conv(in_features, out_features, heads=n_heads, dropout=dropout, concat=concat, edge_dim=edge_dim)
+            self.conv = GATv2Conv(in_features, out_features, heads=n_heads, dropout=dropout, concat=concat, edge_dim=edge_dim, aggr='mean')
 
     def forward(self, x, edge_index, edge_attr=None):
         x = self.conv(x, edge_index, edge_attr)
